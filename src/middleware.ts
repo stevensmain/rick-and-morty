4bb1ccb2661c809cd5088eec84f36d9e664bb458
Stivens Carrasquel <stevensgoldzyzz@gmail.com>
2024-06-09 23:18:58 -0400

    Config NextAuth middleware

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest } from "next/server";
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
+import { AUTH_ROOT, AUTH_ROUTES, DEFAULT_REDIRECT } from "@/routes";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth(async function middleware(req: NextRequest) {
+  const { nextUrl } = req;
+
+  const isAuthenticated = !!req.auth;
+  const isAuthRoute = AUTH_ROUTES.includes(nextUrl.pathname);
+
+  if (isAuthRoute && isAuthenticated)
+    return Response.redirect(new URL(DEFAULT_REDIRECT, nextUrl));
+
+  if (!isAuthenticated && !isAuthRoute)
+    return Response.redirect(new URL(AUTH_ROOT, nextUrl));
+});
---
+++ src/routes.ts
@@ -0,0 +1,3 @@
+export const AUTH_ROOT = "/login";
+export const AUTH_ROUTES = ["/login", "/register"];
+export const DEFAULT_REDIRECT = "/";
