ed517c8a5f243115b0fb00f7c97f052e2baab26a
Stivens Carrasquel <stevensgoldzyzz@gmail.com>
2024-06-11 00:30:59 -0400

    get characters fetch refactor

--- src/app/(root)/characters/page.tsx
+++ src/app/(root)/characters/page.tsx
@@ -6,37 +6,16 @@ export const metadata: Metadata = {
   title: "Characters | Rick And Morty Project",
 };
 
-async function getCharacters(query: string): Promise<ApiResponse<Character>> {
-  const response = await fetch(
-    `https://rickandmortyapi.com/api/character/?${query}`
-  );
+async function getCharacters(): Promise<ApiResponse<Character>> {
+  const response = await fetch(`https://rickandmortyapi.com/api/character`);
   if (!response.ok) {
-    return {
-      info: {
-        count: 0,
-        pages: 0,
-        next: null,
-        prev: null,
-      },
-      results: [],
-    };
+    throw new Error("Failed to fetch characters");
   }
   return response.json();
 }
 
-export default async function CharactersPage({
-  searchParams,
-}: {
-  searchParams: {
-    page?: string;
-    name?: string;
-    species?: string;
-    gender?: string;
-    status?: string;
-  };
-}) {
-  const searchQuery = new URLSearchParams(searchParams).toString();
-  const { results } = await getCharacters(searchQuery);
+export default async function CharactersPage() {
+  const { results } = await getCharacters();
 
   return (
     <>
