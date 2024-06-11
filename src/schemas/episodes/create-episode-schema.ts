1b6a074400597f99cce914aedd7582dd67e21dfe
Stivens Carrasquel <stevensgoldzyzz@gmail.com>
2024-06-10 23:37:05 -0400

    Update episode schema

--- src/schemas/episodes/create-episode-schema.ts
+++ src/schemas/episodes/create-episode-schema.ts
@@ -3,6 +3,7 @@ import * as yup from "yup";
 export const CreateEpisodeSchema = yup.object({
   name: yup.string().required(),
   air_date: yup.string().required(),
+  season: yup.string().required(),
   episode: yup.string().required(),
 });
 
